4ea1e76b1dc45137d3413e450231e087472cb28f
Unknown <kajgore@vp.pl>
2019-11-10 14:52:44 +0100

    Spring boot works

---
+++ src/backend/src/main/java/FoodHero/TestController.java
@@ -0,0 +1,13 @@
+package FoodHero;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index(){
+        return "No witam witam";
+    }
+}
