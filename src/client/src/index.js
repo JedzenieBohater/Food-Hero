50fb544331bf8dcc534485b6e6feba4469d15718
MateuszCiupa <mateuszciupa1998@gmail.com>
2019-11-14 16:00:20 +0100

    dev ready to use

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,2 +1,6 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './css/index.css'
+
+ReactDOM.render(<App />, document.getElementById("root"));
