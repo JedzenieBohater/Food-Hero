a609fe74624f0fc9c34abfab078f407cbfacdc24
MateuszCiupa <01131212@pw.edu.pl>
2019-12-18 22:00:11 +0100

    register component changes

--- src/client/src/components/layouts/Register/index.js
+++ src/client/src/components/layouts/Register/index.js
@@ -1,102 +1,94 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { register } from '../../../actions/session'
+import { Link, Redirect } from 'react-router-dom'
+import { register } from '../../../utils/session'
 
-const mapStateToProps = ({ errorReducer }) => ({
-  errors: errorReducer
-})
-
-const mapDispatchToProps = dispatch => ({
-  register: user => dispatch(register(user))
-})
-
-const Register = ({ errors, register }) => {
+export default () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [password2, setPassword2] = useState('')
   const [isPassword2Valid, setisPassword2Valid] = useState(true)
+  const [redirect, setRedirect] = useState(false)
 
-  const handleSubmit = event => {
+  const handleSubmit = async event => {
     event.preventDefault()
-    register({ email, password })
+    try {
+      await register({ email, password })
+      setRedirect(true)
+    } catch(err) {
+      console.log(err)
+    }
   }
 
   return (
-    <div className="content-box-middle">
-      <form className="content-box">
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <label htmlFor="email">Adres email:</label>
-              </td>
-              <td>
-                <input onChange={e => setEmail(e.target.value)} type="email" id="email" placeholder="email" />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="password">Hasło:</label>
-              </td>
-              <td>
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="hasło"
-                  className="valid"
-                  onChange={e => {
-                    setPassword(e.target.value)
-                  }}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="password2">Powtórz hasło:</label>
-              </td>
-              <td>
-                <input
-                  type="password"
-                  id="password2"
-                  placeholder="powtórz hasło"
-                  className={isPassword2Valid ? '' : 'invalid'}
-                  onChange={e => {
-                    setPassword2(e.target.value)
-                    console.log(password)
-                    console.log(password2)
-                    if (password !== e.target.value) {
-                      setisPassword2Valid(false)
-                    } else {
-                      setisPassword2Valid(true)
-                    }
-                    console.log(isPassword2Valid)
-                  }}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td colSpan="2">
-                <button
-                  className="btn-blue"
-                  onClick={handleSubmit}
-                  disabled={isPassword2Valid ? '' : 'disabled'}
-                >
-                  Zarejestruj
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <div className="middle">
-          <Link to="/login">Zaloguj się</Link>
-        </div>
-      </form>
-    </div>
+    <>
+      {redirect ? <Redirect to='/login' /> : null}
+      <div className="content-box-middle">
+        <form className="content-box">
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <label htmlFor="email">Adres email:</label>
+                </td>
+                <td>
+                  <input onChange={e => setEmail(e.target.value)} type="email" id="email" placeholder="email" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label htmlFor="password">Hasło:</label>
+                </td>
+                <td>
+                  <input
+                    type="password"
+                    id="password"
+                    placeholder="hasło"
+                    className="valid"
+                    onChange={e => {
+                      setPassword(e.target.value)
+                    }}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label htmlFor="password2">Powtórz hasło:</label>
+                </td>
+                <td>
+                  <input
+                    type="password"
+                    id="password2"
+                    placeholder="powtórz hasło"
+                    className={isPassword2Valid ? '' : 'invalid'}
+                    onChange={e => {
+                      setPassword2(e.target.value)
+                      if (password !== e.target.value) {
+                        setisPassword2Valid(false)
+                      } else {
+                        setisPassword2Valid(true)
+                      }
+                    }}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2">
+                  <button
+                    className="btn-blue"
+                    onClick={handleSubmit}
+                    disabled={isPassword2Valid ? '' : 'disabled'}
+                  >
+                    Zarejestruj
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="middle">
+            <Link to="/login">Zaloguj się</Link>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Register)
