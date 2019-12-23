3831eb241f10a8f8088517f46903c569012d6a61
matsmolinski <matsmolinski@gmail.com>
2019-12-23 19:20:34 +0100

    Rewriting App.js with updated language swap

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -2,21 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Header, Login, Register, ForgottenPassword, Home } from './layouts'
 import { AuthRoute } from '../utils/route'
-import counterpart from 'counterpart'
 import {en, pl} from '../language/index'
 
 
-
-counterpart.registerTranslations('pl', pl)
-counterpart.registerTranslations('en', en)
-counterpart.setLocale('en')
 
 export default class App extends React.Component {
   constructor(props){
     super(props)
 
     this.state = {
-      lang : counterpart.getLocale(),
+      lang : en,
       translation: en
     }
 
@@ -39,13 +34,12 @@ export default class App extends React.Component {
       lang: e.target.value,
       translation: language
     })
-    counterpart.setLocale(e.target.value)
   }
 
   render() {
     return (
       <Router>
-        <Header translation={this.state.translation.header} lang={counterpart.getLocale()} onLangChange={this.onLangChange}/>
+        <Header translation={this.state.translation.header} lang={this.state.lang} onLangChange={this.onLangChange}/>
         <Switch>
           <Route exact path="/" component={() => <Home />} />
           <AuthRoute path='/login' component={() => <Login translation={this.state.translation.login}/>} />
