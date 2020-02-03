dbaeb4c891bb46930615500d0c74d231028567b3
Bartek <siwybar@gmail.com>
2020-02-03 11:39:57 +0100

    fetch data

--- src/client/src/components/layouts/Search/index.js
+++ src/client/src/components/layouts/Search/index.js
@@ -1,24 +1,58 @@
-import React from 'react'
+import React,{ useState,useEffect } from 'react'
 import List from './List'
-import data from './exampledata.json'
+import a from './exampledata.json'
 
-const offers = data.offers;
+const data = a.offers;
 
-export default () => {
+export default () => {  const [data, setData] = useState(undefined)
+
+  function getData(url){
+    fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      setData(myJson.offers);
+    });
+  }
+
+  useEffect(() => {
+    getData('http://192.168.99.100:5001/js');
+  },[]);
+  
   return (
-    <div className="searchmain">
-    <div className='content-box col25'>Wyszukiwarka</div>
-    <div className='content-box col75'>
-        {offers.map( offer => (<List
+    <div>
+    <div className='content-box searchmain flexing search'>
+    <label htmlFor="what">Czego szukasz: </label>
+    <input id='what' type='text'/>
+    <label htmlFor="localization">Lokalizacja: </label>
+    <input id='localization' type='text'/>
+    <button className="btn-blue">Szukaj</button>
+    </div>
+    <div className="searchmain flexing">
+    <div className='content-box col25'>filtry</div>
+    <div className='content-box col75 centering'>
+      {data ? 
+        data.map( offer => (<List
+        key={offer.id}
         id={offer.id}
         picture={offer.picture}
         title={offer.title} 
-        cook={offer.cook}
-        date={offer.date}
-        location={offer.location}
-        grade={offer.grade}
-        description={offer.description}
-        ></List>))}
+        wystawiajacy={offer.wystawiajacy}
+        data={offer.data}
+        lokalizacja={offer.lokalizacja}
+        ocena={offer.ocena}
+        opis={offer.opis}
+        ></List>))
+      :  
+      <div id="dots1">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+      </div>
+      }
+    </div>
     </div>
     </div>
   )
