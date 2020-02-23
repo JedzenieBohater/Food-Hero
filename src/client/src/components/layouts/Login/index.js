2848af7b79da66084f14265ae36dec3c36a9b09e
MateuszCiupa <48243124+MateuszCiupa@users.noreply.github.com>
2020-02-23 20:11:40 +0100

    bcrypt not needed on frontend [ci skip]

--- src/client/src/components/layouts/Login/index.js
+++ src/client/src/components/layouts/Login/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { login } from '../../../actions/session'
-import bcrypt from 'bcryptjs'
 import PropTypes from 'prop-types'
 
 
