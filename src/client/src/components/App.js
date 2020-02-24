4fc5eb66690d70c4d2025ca65aaba78cfb8860c0
MateuszCiupa <48243124+MateuszCiupa@users.noreply.github.com>
2020-02-24 18:32:02 +0100

    Update App.js

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Header, Login, Register, ForgottenPassword, Home,Search, Profile} from './layouts'
-import { AuthRoute } from '../utils/route'
-
-
+import { Header, Login, Register, ForgottenPassword, Home, Search, Profile} from './layouts'
+import { AuthRoute } from 'utils/route'
 
 export default () => (
   <Router>
