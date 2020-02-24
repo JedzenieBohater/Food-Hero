a04b1f05f26bb1637cbee25ba4ab0efc9d51f828
MateuszCiupa <01131212@pw.edu.pl>
2020-02-24 14:04:16 +0100

    protected route added

--- src/client/src/utils/route.js
+++ src/client/src/utils/route.js
@@ -2,15 +2,38 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Redirect, Route } from 'react-router-dom'
 
+const Protected = ({ loggedIn, path, component: Component, exact }) => (
+  <Route 
+    path={path}
+    exact={Boolean(exact)}
+    render={props => 
+      loggedIn ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+)
+
+const Auth = ({ loggedIn, path, component: Component, exact }) => (
+  <Route
+    path={path}
+    exact={Boolean(exact)}
+    render={props =>
+      loggedIn ? (
+        <Redirect to="/" />
+      ) : (
+        <Component {...props} />
+      )
+    }
+  />
+)
+
 const mapStateToProps = ({ sessionReducer: { userID } }) => ({
   loggedIn: Boolean(userID)
 })
 
-const Auth = ({ loggedIn, path, component: Component }) => (
-  <Route
-    path={path}
-    render={props => (loggedIn ? <Redirect to='/' /> : <Component {...props} />)} 
-  />
-)
+export const ProtectedRoute = connect(mapStateToProps)(Protected)
 
 export const AuthRoute = connect(mapStateToProps)(Auth)
