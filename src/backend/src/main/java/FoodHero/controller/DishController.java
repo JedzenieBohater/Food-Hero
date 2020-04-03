e099bd2ef0e70ea06487b4007defa9099e36af7a
Unknown <kajgore@vp.pl>
2020-04-03 22:22:18 +0200

    Tests

--- src/backend/src/main/java/FoodHero/controller/DishController.java
+++ src/backend/src/main/java/FoodHero/controller/DishController.java
@@ -2,6 +2,7 @@ package FoodHero.controller;
 
 import FoodHero.model.Dish;
 import FoodHero.service.Dish.DishService;
+import FoodHero.service.Login.LoginService;
 import FoodHero.service.Utils.ReturnCode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.Map;
 
 @RestController
@@ -19,26 +21,39 @@ import java.util.Map;
 public class DishController {
 
     DishService dishService;
+    LoginService loginService;
     private static final Logger LOGGER = LogManager.getLogger(DishController.class);
 
 
     @Autowired
-    public DishController(DishService dishService){
+    public DishController(DishService dishService, LoginService loginService) {
         this.dishService = dishService;
+        this.loginService = loginService;
     }
 
     @PostMapping(value = "/{id}")
-    public ResponseEntity<Object> createDish(@PathVariable("id") int id, @RequestBody Map<String, Object> payload) {
-        if (payload == null) {
-            return new ResponseEntity<>(ReturnCode.MISSING_ARG.toString() + "\nLack of json payload.", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> createDish(@PathVariable("id") int id, @RequestBody Map<String, Object> payload, Principal principal) {
+        int userID = 0;
+        if (principal != null) {
+            userID = loginService.getIdByEmail(principal.getName());
+        } else {
+            userID = -1;
         }
-        ReturnCode returnCode = dishService.createDish(id, payload);
-        if (returnCode == ReturnCode.NOT_FOUND) {
-            return new ResponseEntity<>(ReturnCode.NOT_FOUND.toString() + "\nAccount not found", HttpStatus.NOT_FOUND);
-        } else if (returnCode == ReturnCode.INCORRECT_DATA) {
-            return new ResponseEntity<>(ReturnCode.INCORRECT_DATA.toString() + "\nWrong json payload.", HttpStatus.BAD_REQUEST);
+
+        if ((userID != -1 && userID == id) || (loginService.getLogin(userID).isPresent() && loginService.getLogin(userID).get().getIs_admin())) {
+            if (payload == null) {
+                return new ResponseEntity<>(ReturnCode.MISSING_ARG.toString() + "\nLack of json payload.", HttpStatus.BAD_REQUEST);
+            }
+            ReturnCode returnCode = dishService.createDish(id, payload);
+            if (returnCode == ReturnCode.NOT_FOUND) {
+                return new ResponseEntity<>(ReturnCode.NOT_FOUND.toString() + "\nAccount not found", HttpStatus.NOT_FOUND);
+            } else if (returnCode == ReturnCode.INCORRECT_DATA) {
+                return new ResponseEntity<>(ReturnCode.INCORRECT_DATA.toString() + "\nWrong json payload.", HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(ReturnCode.OK.toString() + "\nDish created successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(ReturnCode.NO_ACCESS.toString() + "\nYou have no permissions to modify account", HttpStatus.FORBIDDEN);
         }
-        return new ResponseEntity<>(ReturnCode.OK.toString() + "\nDish created successfully", HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}", produces = "application/json")
@@ -49,14 +64,14 @@ public class DishController {
         }
         return new ResponseEntity<>(dish, HttpStatus.OK);
     }
-
+    //TODO tutaj trzeba zabezpieczenia
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Object> updateDish(@PathVariable("id") int id, Map<String, Object> payload){
+    public ResponseEntity<Object> updateDish(@PathVariable("id") int id, Map<String, Object> payload) {
         if (payload == null) {
             return new ResponseEntity<>(ReturnCode.MISSING_ARG.toString() + "\nLack of json payload.", HttpStatus.BAD_REQUEST);
         }
         ReturnCode returnCode = dishService.updateDish(id, payload);
-        if(returnCode == ReturnCode.NOT_FOUND){
+        if (returnCode == ReturnCode.NOT_FOUND) {
             return new ResponseEntity<>(ReturnCode.NOT_FOUND.toString() + "\nDish not found", HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(ReturnCode.OK.toString() + "\nDish updated successfully", HttpStatus.OK);
